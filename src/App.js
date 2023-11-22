90ec3e38f06d5331b2fd7f2337da958045ef45ea
ProbablyAnOcelot <ProbablyAnOcelot@gmail.com>
2023-11-22 03:17:32 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          class="bg-sky-500 hover:bg-sky-700 ..."
+          // className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
